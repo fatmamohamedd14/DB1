922bcf18eb448cc688524b6cb42d97b2ce96530f
Abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2024-05-02 21:08:19 +0300

    ci: install packages for dev and fix error in react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {DarkModeContextProvider} from "./context/darkModeContext";
-ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeContextProvider> 
-     <App />
-     </DarkModeContextProvider> 
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
+// Define a function to render your app
+const renderApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </React.StrictMode>
+  );
+};
 
- 
+// Call the function to render the app initially
+renderApp();
+
+// Warn if ReactDOM.render is used in React 18
+if (React.version.startsWith("18")) {
+  console.warn(
+    "ReactDOM.render is used in React 18. Consider migrating to createRoot. Learn more: https://reactjs.org/link/switch-to-createroot"
+  );
+}

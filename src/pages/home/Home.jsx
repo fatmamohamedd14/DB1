669350f77a090608738efd4cc0c6a10316c5aece
Abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2024-05-08 00:15:54 +0300

    not signed in logic

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,32 @@
-import React from "react";
-import "./Home.scss"
+import React, { useEffect } from "react";
+import "./Home.scss";
 import Sidebar from "../../component/sidebar/Sidebar";
 import Navbar from "../../component/navbar/Navbar";
 import Widget from "../../component/widget/Widget";
-
 import Chart from "../../component/chart/Chart";
-
-const Home =() => {
-    return(
-        <div className="home">
-            <Sidebar/>
-            <div className="homeContainer">
-            <Navbar/>
-            <div className="widgets">
-                  <Widget type="user"/>
-                  <Widget type="book"/>
-                  <Widget type="auther"/>
-            </div>
-            <div className="charts">
-                <Chart/>
-            </div>
-            </div>     
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    }
+  }, [navigate]);
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="book" />
+          <Widget type="auther" />
         </div>
-    )
-}
-export default Home
+        <div className="charts">
+          <Chart />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
